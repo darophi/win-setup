b58affa91ed16dccf630287f413f969513acd0c9
Raphael Neumann <work@raphaelneumann.dk>
2023-06-01 20:22:03 +0200

    Git init

---
+++ dev-deps.ps1
@@ -0,0 +1,8 @@
+winget install -h --accept-package-agreements Microsoft.VisualStudioCode
+winget install -h --accept-package-agreements JetBrains.Rider.EAP
+winget install -h --accept-package-agreements Docker.DockerDesktop
+winget install -h --accept-package-agreements Microsoft.DotNet.SDK.7
+winget install -h --accept-package-agreements Microsoft.DotNet.SDK.6
+winget install -h --accept-package-agreements Fork.Fork
+winget install -h --accept-package-agreements Git.Git
+winget install -h --accept-package-agreements Notepad++.Notepad++
---
+++ utils-deps.ps1
@@ -0,0 +1,4 @@
+winget install -h --accept-package-agreements Notion.Notion
+winget install -h --accept-package-agreements Spotify.Spotify
+winget install -h --accept-package-agreements Figma.Figma
+winget install -h --accept-package-agreements WinDirStat.WinDirStat
